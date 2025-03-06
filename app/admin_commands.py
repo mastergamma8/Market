a27f5b68b6af5a126a67f05dba610932235e9973
ThDewid <99511120+mastergamma8@users.noreply.github.com>
2025-03-06 13:03:38 +0200

    Update admin_commands.py

--- app/admin_commands.py
+++ app/admin_commands.py
@@ -16,6 +16,7 @@ from typing import Tuple
 import exchange_commands
 from auctions import router as auctions_router, register_auction_tasks
 from offer import router as offer_router
+from aiogram.filters import Command
 # Импорт роутера из exchange_web
 from exchange_web import router as exchange_router
 
